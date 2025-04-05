3593c237b3a0050becd02a78c0d808c2cfdbe282
igormel0 <igor_oliveira_melo@hotmail.com>
2025-04-04 22:14:27 -0300

    Add files via upload

---
+++ ListaPV01Questao01/src/listapv01questao01/ListaPV01Questao01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listapv01questao01;
+
+/**
+ *
+ * @author igor
+ */
+public class ListaPV01Questao01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JanelaCliques janelaCliques = new JanelaCliques();
+        janelaCliques.setVisible(true);
+    }
+    
+}
---
+++ ListaPVQuestao02/src/listapvquestao02/ListaPVQuestao02.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listapvquestao02;
+
+/**
+ *
+ * @author igor
+ */
+public class ListaPVQuestao02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaCor telaCor = new TelaCor();
+        telaCor.setVisible(true);
+    }
+    
+}
